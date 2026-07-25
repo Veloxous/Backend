bc2d9b2d94e773c6b086b1590ba6d15dcd7cfdf8
Adewale Iyanuoluwa Isaac <hizick27@gmail.com>
2026-07-25 21:34:18 +0100

    Setup DB layer and add pg dependencies

---
+++ src/db/db.ts
@@ -0,0 +1,26 @@
+import { Pool, PoolClient } from "pg";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+
+// Helper for atomic database transactions
+export async function withTransaction<T>(
+  callback: (client: PoolClient) => Promise<T>
+): Promise<T> {
+  const client = await pool.connect();
+  try {
+    await client.query("BEGIN");
+    const result = await callback(client);
+    await client.query("COMMIT");
+    return result;
+  } catch (error) {
+    await client.query("ROLLBACK");
+    throw error;
+  } finally {
+    client.release();
+  }
+}
